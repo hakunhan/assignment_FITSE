c98c23dc53b2daea631012054b72ace2153dffaf
hakunhan <hakunhan@gmail.com>
2020-10-26 08:49:26 +0700

    Update assignment2

--- a1_1801040081/src/a2_1801040081/SortedSet.java
+++ a1_1801040081/src/a2_1801040081/SortedSet.java
@@ -1,4 +1,28 @@
 package a2_1801040081;
+import java.lang.Comparable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Vector;
 
-public class SortedSet {
+/**
+ * @overview a collection data type that the objects
+ * are sorted in ascending order
+ * @attribute
+ * elements SortedSet<Comparable> Vector<Comparable>
+ * @object
+ * A typical SortedSet is SortedSet = {elements} where
+ * elements(elements)
+ * @abstract_properties
+ * mutable(elements) = true /\ optional(elements) = false
+ */
+public class SortedSet<Comparable> {
+    private Vector<Comparable> elements;
+
+    public int size() {
+        return 0;
+    }
+
+    public Iterator iterator() {
+        return null;
+    }
 }
