ebefd3e0faac7c7a0e536aaf4859e6a801643ed9
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-11-01 21:29:05 +0700

    Update code

--- a1_1801040081/src/a2_1801040081/test.java
+++ a1_1801040081/src/a2_1801040081/test.java
@@ -5,15 +5,21 @@ import java.util.Iterator;
 
 public class test {
     public static void main(String[] args) {
+        Customer customer1 = new Customer(1, "a", "012", "abc");
         Comparable c1 = new Customer(1, "a", "012", "abc");
         Comparable c2 = new Customer(2, "abc", "123", "def");
+        Comparable c3 = new Customer(1, "a", "012", "abc");
+
+
 
         SortedSet s = new SortedSet();
         s.insert(c2);
         s.insert(c1);
-        s.getElement(3);
+        s.insert(c3);
+        System.out.println(s.toString());
 
-        Iterator a = s.iterator();
+
+        Iterator a = s.iter;
         System.out.println(a.hasNext());
         System.out.println(a.next().toString());
     }
